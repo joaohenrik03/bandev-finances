844a45da735a28151f1d403a30783a61491272de
João Henrik Signori Zilch <84729916+joaohenrik03@users.noreply.github.com>
2022-05-05 18:55:50 -0300

    Update index.js

--- index.js
+++ index.js
@@ -58,9 +58,11 @@ const DOM = {
         DOM.transactionsContainer.appendChild(tr);
     },
     innerHTMLTransaction(transaction) {
+        const CSSclass = transaction.amount > 0 ? 'income' : 'expense';
+
         const html = `
             <td class="description">${transaction.description}</td>
-            <td class="expense">${transaction.amount}</td>
+            <td class="${CSSclass}">${transaction.amount}</td>
             <td class="date">${transaction.date}</td>
             <td>
                 <img src="./assets/minus.svg" alt="Remover transação">
