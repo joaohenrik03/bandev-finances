be7d5431fb8e7709fcd3776db58cfde178f6aa39
João Henrik Signori Zilch <84729916+joaohenrik03@users.noreply.github.com>
2022-05-05 19:07:56 -0300

    Update index.js

--- index.js
+++ index.js
@@ -60,9 +60,11 @@ const DOM = {
     innerHTMLTransaction(transaction) {
         const CSSclass = transaction.amount > 0 ? 'income' : 'expense';
 
+        const amount = Utils.formatCurrency(transaction.amount)
+
         const html = `
             <td class="description">${transaction.description}</td>
-            <td class="${CSSclass}">${transaction.amount}</td>
+            <td class="${CSSclass}">${amount}</td>
             <td class="date">${transaction.date}</td>
             <td>
                 <img src="./assets/minus.svg" alt="Remover transação">
@@ -70,6 +72,21 @@ const DOM = {
         `;
 
         return html;
+    }
+};
+
+const Utils = {
+    formatCurrency(value) {
+        const signal = Number(value) < 0 ? '-' : '';
+
+        value = String(value).replace(/\D/g, '');
+        value = Number(value) / 100;
+        value = value.toLocaleString('pt-br', {
+            style: 'currency',
+            currency: 'BRL'
+        });
+
+        return signal + value;
     }
 };
 
