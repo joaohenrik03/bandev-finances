b44ea4fe3e2c1a75491e2e73f1102826a005abc8
João Henrik Signori Zilch <84729916+joaohenrik03@users.noreply.github.com>
2022-05-06 14:31:51 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-// Abrir e fechar modal
-// Open and Close modal
-
 const Modal = {
     open() {
         document.querySelector('.modal-overlay').classList.add('show');
@@ -72,11 +69,12 @@ const DOM = {
     transactionsContainer: document.querySelector('#data-table tbody'),
     addTransaction(transaction, index) {
         const tr = document.createElement('tr');
-        tr.innerHTML = DOM.innerHTMLTransaction(transaction);
+        tr.innerHTML = DOM.innerHTMLTransaction(transaction, index);
+        tr.dataset.index = index
 
         DOM.transactionsContainer.appendChild(tr);
     },
-    innerHTMLTransaction(transaction) {
+    innerHTMLTransaction(transaction, index) {
         const CSSclass = transaction.amount > 0 ? 'income' : 'expense';
 
         const amount = Utils.formatCurrency(transaction.amount)
@@ -86,7 +84,7 @@ const DOM = {
             <td class="${CSSclass}">${amount}</td>
             <td class="date">${transaction.date}</td>
             <td>
-                <img src="./assets/minus.svg" alt="Remover transação">
+                <img onclick="Transaction.remove(${index})" src="./assets/minus.svg" alt="Remover transação">
             </td>
         `;
 
@@ -193,8 +191,8 @@ const Form = {
 
 const App = {
     start() {
-        Transaction.all.forEach((transaction) => {
-            DOM.addTransaction(transaction);
+        Transaction.all.forEach((transaction, index) => {
+            DOM.addTransaction(transaction, index);
         });
 
         DOM.updateBalance(); 
